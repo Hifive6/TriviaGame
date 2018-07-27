676b3651d99e128169bbc3d619bfe92d51aeda13
Hi56 <39879994+Hifive6@users.noreply.github.com>
2018-07-27 11:53:08 -0400

    made object to hold Q's and A's

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//I will start with writing an object for holding all the questions and the answers for the game.
+//STarted with 3 questions to make it simpler I will add more as I continue
+var question = {
+    q1: "Who is The Elven Sword-Wielding, Green Clothed Hero in the Legend of Zelda series?",
+    a1: "Link",
+    q2: "What is the name of the primary protangist of the God of War Series?",
+    a2: "Kratos",
+    q3: "In the war game Homefront, who is the enemy Country?",
+    a3: "North Korea"
+};
+
+
+
+
+
+// function myFunction() {
+//     var startBtn = document.getElementByClassName("begin");
+//     if (startBtn.style.display === "none") {
+//         startBtn.style.display = "block";
+//     } else {
+//         startBtn.style.display = "none";
+//     }
+// }
