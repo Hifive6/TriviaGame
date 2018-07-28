067666edfe4d3b88a47ab718534caa0305ead2c5
Hi56 <39879994+Hifive6@users.noreply.github.com>
2018-07-28 16:32:50 -0400

    added all I think html elements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,21 @@
 //I will start with writing an object for holding all the questions and the answers for the game.
 //STarted with 3 questions to make it simpler I will add more as I continue
-var questions = {
-    q1: ["Who is The Elven Sword-Wielding, Green Clothed Hero in the Legend of Zelda series?"],
-    a1: ["Link ", " Zelda ", ' Peter Pan'],
-    q2: ["What is the name of the primary protangist of the God of War Series?", "Kratos"],
-    a2: ["Zeus ", " Atreus ", " Kratos"],
-    q3: ["In the war game Homefront, who is the enemy Country?", "North Korea"],
-    a3: ["Russia ", " North Korea ", " China"],
-};
+var questions = [{
+    question: ["Who is The Elven Sword-Wielding, Green Clothed Hero in the Legend of Zelda series?"],
+    answerChoices: ["Link,   ", "  Zelda,  ", "  Peter Pan"],
+    answer: 0,
+},{
+
+    question: ["What is the name of the primary protangist of the God of War Series?", "Kratos"],
+    answerChoices: ["Zeus ", " Atreus ", " Kratos"],
+    answer: 2,
+},{
+    question: ["In the war game Homefront, who is the enemy Country?", "North Korea"],
+    answer: ["Russia ", " North Korea ", " China"],
+    answer: 1,
+}
+];
+
 
 
 //These are the variable that will hold my answers and time limit
@@ -16,48 +24,16 @@ var correct;
 var wrong;
 var unanswered;
 var intervalId;
-var questions;
+var activeQuestion;
+var selectAnswer;
+
+
 
 //This function will start my game
 function startGame() {
 
+
 };
-function run(){
-    clearInterval(intervalId);
-    intervalID = setInterval(decrement, 1000 + 1);
-}
-function decrement(){
-    time--;
-    $("#counter").html("<h1>" + time + "<h1>");
-    if(time === 0){
-        stop(),
-        alert ("next Question");
-    }
-
-    function stop(){
-        clearInterval(intervalID);
-    }
-    
-
- 
-
-
-
-// This function is used to make the button disappear when clicked and the questions will pop up in its palce
- document.getElementById('begin').addEventListener("click", myFunction);
-function myFunction() {
-    var beginGame = document.getElementById("begin");
-    if (beginGame.style.display === "none") {
-        beginGame.style.display = "block";
-    } else {
-        beginGame.style.display = "none";
-    }
-    //this is just a test, i will replace with first question or questions
-    $("#replace").html(questions.q1 + "<div><br>" + questions.a1);
-    run();
-    decrement();
-    
-}
 
 //Created a counter for my questions
 function run(){
@@ -75,7 +51,26 @@ function decrement(){
     function stop(){
         clearInterval(intervalID);
     }
+}   
+
+ 
+
+
+
+// This function is used to make the button disappear when clicked and the questions will pop up in its palce
+ document.getElementById('begin').addEventListener("click", myFunction);
+function myFunction() {
+    var beginGame = document.getElementById("begin");
+    if (beginGame.style.display === "none") {
+        beginGame.style.display = "block";
+    } else {
+        beginGame.style.display = "none";
+    }
+    //this is just a test, i will replace with first question or questions
+    $("#question").html(questions[0].question1 + " <hr><br>" + questions[0].answerChoices1);
     
-
-
-}
+   
+    run();
+    decrement();
+    
+}   
